569a95da6bbcbe76a8d8af27a772691959ede579
LaviniaPop31 <laviniapop31@gmail.com>
2021-05-23 22:08:21 +0300

    changed based on feedback

--- s5-hooks/src/components/PostView/postView.js
+++ s5-hooks/src/components/PostView/postView.js
@@ -5,14 +5,13 @@ import Comments from '../Comments/comments'
 export default function PostView({id, title, body, userId}) {
   const [showComments, setShowComments] = useState('false')
  
-  let emptyArray = []
-
   return (
     <div style={{textAlign: "left"}}>
         <h4>Title: {title}</h4>
         <p>Content: {body}</p>
         <button onClick={() => {setShowComments(!showComments)}}>Comments</button>
-        {(!showComments) ? <Comments  id={id}/> : emptyArray }
+        {(!showComments) ? <Comments  id={id}/> : null } 
+        {/* or {showComments && <Comments id={id} />} */}
     </div>
   );
 }
